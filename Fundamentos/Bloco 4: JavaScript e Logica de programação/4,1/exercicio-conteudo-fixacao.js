af29e34b3ec41d1db02d033f263fbf893d001c2d
Thivasaki <thiagoivasaki@gmail.com>
2022-05-12 16:05:41 -0300

    Finalizado exercícios fixação conteúdo

---
+++ Fundamentos/Bloco 4: JavaScript e Logica de programação/4,1/exercicio-conteudo-fixacao.js
@@ -0,0 +1,8 @@
+const myName = "Thiago Seichy Ivasaki"
+const birthCity = "Formosa"
+var birthYear = 2000
+birthYear = 2030
+
+console.log (myName)
+console.log (birthCity)
+console.log (birthYear)
---
+++ Fundamentos/Bloco 4: JavaScript e Logica de programação/4,1/exercicio-switch-case.js
@@ -0,0 +1,18 @@
+var candidato = "reprovado"
+
+switch (candidato) {
+    case "aprovado":
+        console.log("Parabéns");
+        break;
+    
+    case "lista":
+        console.log("Ahuarde");
+        break;
+    
+    case "reprovado":
+        console.log("Não foi");
+        break;
+default:
+console.log ("sei la se vira")
+    
+}
